c7b0ed79f5fec7258d840aa3b21d8474a47d5086
wh0ami <misha@marinenko.ml>
2017-10-05 23:42:40 +0300

    CoreCoin SeedPeer added

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -105,7 +105,7 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-  "127.0.0.1:65534",
+  "149.154.69.14:65534",
   //"your_seed_ip2.com:8080",
 };
 
